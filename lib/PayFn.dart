6116cbac9e70625fd752902686af6609323e2f86
irajeshh <85046336+irajeshh@users.noreply.github.com>
2025-03-05 18:56:46 +0530

    updated for wasm 👍

--- lib/PayFn.dart
+++ lib/PayFn.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
-import 'dart:js' as js;
-import 'dart:js_interop';
-import 'dart:js_interop_unsafe';
+import 'dart:collection';
+// import 'dart:js' as js;
+import 'dart:js_interop' as jsinterop;
+import 'dart:js_interop_unsafe' as jsinterop_unsafe;
 
-// import 'dart:js_interop' as js;
+import 'package:flutter/foundation.dart';
 import 'package:web/web.dart' as web;
 
 class PayFn {
@@ -25,22 +26,32 @@ class PayFn {
 
     // Ensure Razorpay SDK is loaded before proceeding
     // bool isRazorpayLoaded = js.context.hasProperty('Razorpay');
-    bool isRazorpayLoaded = web.window.hasProperty('Razorpay'.toJS).toDart;
+    // bool isRazorpayLoadedMethod1 = web.window.hasProperty('Razorpay'.toJS).toDart;
+    bool isRazorpayLoaded = web.window.has('Razorpay');
+
     if (isRazorpayLoaded == false) {
       completer.completeError("Razorpay SDK not loaded");
       return completer.future;
     }
 
-    void handlerFn(response) {
-      returnMap['type'] = _CODE_PAYMENT_SUCCESS;
-      dataMap['razorpay_payment_id'] = response['razorpay_payment_id'];
-      dataMap['razorpay_order_id'] = response['razorpay_order_id'];
-      dataMap['razorpay_signature'] = response['razorpay_signature'];
-      returnMap['data'] = dataMap;
-      completer.complete(returnMap);
+    void handlerFn(jsinterop.JSObject jsResponse) {
+      Object? responseDartObject = jsResponse.dartify();
+      debugPrint('response type: ${responseDartObject.runtimeType}');
+      if (responseDartObject != null) {
+        Map response = Map.from(responseDartObject as LinkedHashMap);
+        returnMap['type'] = _CODE_PAYMENT_SUCCESS;
+        dataMap['razorpay_payment_id'] = response['razorpay_payment_id'];
+        dataMap['razorpay_order_id'] = response['razorpay_order_id'];
+        dataMap['razorpay_signature'] = response['razorpay_signature'];
+        returnMap['data'] = dataMap;
+        completer.complete(returnMap);
+      } else {
+        debugPrint('response is not Map');
+      }
     }
 
     void dismissFn() {
+      debugPrint('dismissed');
       if (!completer.isCompleted) {
         returnMap['type'] = _CODE_PAYMENT_ERROR;
         dataMap['code'] = PAYMENT_CANCELLED;
@@ -51,17 +62,24 @@ class PayFn {
     }
 
     // Handle payment failure
-    void onFailedFn(response) {
-      returnMap['type'] = _CODE_PAYMENT_ERROR;
-      dataMap['code'] = BASE_REQUEST_ERROR;
-      dataMap['message'] = response['error']['description'];
-      var metadataMap = <dynamic, dynamic>{};
-      metadataMap['payment_id'] = response['error']['metadata']['payment_id'];
-      dataMap['metadata'] = metadataMap;
-      dataMap['source'] = response['error']['source'];
-      dataMap['step'] = response['error']['step'];
-      returnMap['data'] = dataMap;
-      completer.complete(returnMap);
+    void onFailedFn(jsinterop.JSObject jsResponse) {
+      debugPrint('error onFailedFn');
+      Object? dartObject = jsResponse.dartify();
+      if (dartObject != null) {
+        Map response = Map.from(dartObject as LinkedHashMap);
+        returnMap['type'] = _CODE_PAYMENT_ERROR;
+        dataMap['code'] = BASE_REQUEST_ERROR;
+        dataMap['message'] = response['error']['description'];
+        var metadataMap = <dynamic, dynamic>{};
+        metadataMap['payment_id'] = response['error']['metadata']['payment_id'];
+        dataMap['metadata'] = metadataMap;
+        dataMap['source'] = response['error']['source'];
+        dataMap['step'] = response['error']['step'];
+        returnMap['data'] = dataMap;
+        completer.complete(returnMap);
+      } else {
+        debugPrint('onFailedFn response is not Map');
+      }
     }
 
     options['handler'] = handlerFn;
@@ -69,19 +87,22 @@ class PayFn {
 
     // Initialize Razorpay instance
     /// Converting dart map to js object
-    js.JsObject jsmapFromDart = js.JsObject.jsify(options);
+    // js.JsObject jsmapFromDart = js.JsObject.jsify(options);
+    jsinterop.JSAny? jsmapFromDart = options.jsify();
 
     /// Retrieving Browser Object named Razorpay from the .js file we received from checkout API
-    Object browserObject = js.context.callMethod('Razorpay', [jsmapFromDart]);
+    jsinterop.JSAny? razorpay = web.window.callMethod('Razorpay'.toJS, jsmapFromDart);
 
     /// Converting Browser Object to JS Object
-    js.JsObject razorpay = js.JsObject.fromBrowserObject(browserObject);
+    // js.JsObject razorpay = js.JsObject.fromBrowserObject(browserObject);
 
-    ///Assigning the onFailedFn to the payment.failed event
-    razorpay.callMethod('on', ['payment.failed', onFailedFn]);
+    if (razorpay is jsinterop.JSObject) {
+      ///Assigning the onFailedFn to the payment.failed event
+      razorpay.callMethod('on'.toJS, onFailedFn.toJS);
 
-    ///If no errors captured, then execute the ['open'] method
-    razorpay.callMethod('open');
+      ///If no errors captured, then execute the ['open'] method
+      razorpay.callMethod('open'.toJS);
+    }
     return completer.future;
   }
 }
