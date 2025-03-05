3f7a3a783323bd1f77b569ab254c313a211100ee
irajeshh <85046336+irajeshh@users.noreply.github.com>
2025-03-05 18:53:35 +0530

    Example code improved for failed payments 👍

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:razorpay_web/razorpay_web.dart';
 
@@ -20,15 +21,58 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Razorpay Sample App'),
+        appBar: appBar(),
+        body: body(),
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text('Razorpay Sample App'),
+    );
+  }
+
+  Widget body() {
+    return Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            listtile(true),
+            listtile(false),
+            ElevatedButton(
+              onPressed: openCheckout,
+              child: const Text('Pay'),
+            ),
+          ],
+        ));
+  }
+
+  Widget listtile(bool isSucess) {
+    ///Reference: https://razorpay.com/docs/payments/payments/test-upi-details/
+    String text = isSucess ? 'success@razorpay' : 'failure@razorpay';
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListTile(
+        tileColor: isSucess ? Colors.green.shade50 : Colors.red.shade50,
+        title: Text(text),
+        subtitle: Text(
+          'UPI ID For ${isSucess ? 'successful' : 'failed'} payment',
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
         ),
-        body: Center(
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-              ElevatedButton(onPressed: openCheckout, child: const Text('Open'))
-            ])),
+        trailing: IconButton(
+          onPressed: () async {
+            await Clipboard.setData(ClipboardData(text: text));
+            Fluttertoast.showToast(msg: "Copied to clipboard", toastLength: Toast.LENGTH_SHORT);
+            await Future.delayed((const Duration(seconds: 1))).then((_) {
+              openCheckout();
+            });
+          },
+          tooltip: 'Copy',
+          icon: const Icon(Icons.copy),
+        ),
       ),
     );
   }
@@ -70,22 +114,20 @@ class _MyAppState extends State<MyApp> {
 
   void _handlePaymentSuccess(PaymentSuccessResponse response) {
     log('Success Response: $response');
-    Fluttertoast.showToast(
-        msg: "SUCCESS: ${response.paymentId!}",
-        toastLength: Toast.LENGTH_SHORT);
+    Fluttertoast.showToast(msg: "SUCCESS: ${response.paymentId!}", toastLength: Toast.LENGTH_SHORT);
   }
 
   void _handlePaymentError(PaymentFailureResponse response) {
     log('Error Response: $response');
     Fluttertoast.showToast(
-        msg: "ERROR: ${response.code} - ${response.message!}",
-        toastLength: Toast.LENGTH_SHORT);
+        msg: "ERROR: ${response.code} - ${response.message!}", toastLength: Toast.LENGTH_SHORT, backgroundColor: const Color(0xFFF44336), webBgColor: "linear-gradient(to right, #F44236, #F44336)");
   }
 
   void _handleExternalWallet(ExternalWalletResponse response) {
     log('External SDK Response: $response');
     Fluttertoast.showToast(
-        msg: "EXTERNAL_WALLET: ${response.walletName!}",
-        toastLength: Toast.LENGTH_SHORT);
+      msg: "EXTERNAL_WALLET: ${response.walletName!}",
+      toastLength: Toast.LENGTH_SHORT,
+    );
   }
 }
